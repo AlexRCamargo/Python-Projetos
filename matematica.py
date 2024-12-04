02c396c8b221d6a544e1e9b164a831b60fa4a64d
AlexRCamargo <alexrcamargoti@gmail.com>
2024-12-04 20:52:38 -0300

    Aprendendo Ptython

---
+++ matematica.py
@@ -0,0 +1,32 @@
+# Funções builtin
+
+# valores = [2,5,8-1,0,11,7,-6]
+# print(max(valores))
+# print(min(valores))
+
+# a = -5
+# b = 4
+
+# print(abs(a))
+# print(pow(a, b))
+
+# c = 2.789011
+
+# print(round(c,2))
+
+import math
+
+x = 8
+y = 100
+
+raiz_quadrada = math.sqrt(x)
+print(raiz_quadrada)
+print(round(raiz_quadrada, 2))
+print(math.ceil(raiz_quadrada)) # Arrendonda para casa acima
+print(math.floor(raiz_quadrada)) # Arrendonda para casa abaixo
+
+logaritmo = math.log10(y)
+print(logaritmo)
+print(math.pi)
+print(math.factorial(x))
+print(x / math.inf)
---
+++ strings.py
@@ -0,0 +1,80 @@
+# nome = 'Alexsander'
+# letra = nome[5]
+# print(letra)
+# print(len(nome))
+
+# nome = 'Curso de Python'
+# instrutor = 'Alexsander'
+# print(nome + ' com ' + instrutor)
+
+frase = 'Vamos aprender Python hoje.'
+# palavras = frase.split()
+# print(palavras)
+# print(palavras[1])
+# for palavra in palavras:
+#     print(palavra)
+# for letra in frase:
+#     print(letra)
+# print(frase[0:5])
+
+# email = input('Digite o seu email: ')
+# arroba = email.find('@')
+# print(arroba)
+# usuario = email[0:arroba]
+# dominio = email[arroba+1:]
+# print(usuario)
+# print(dominio)
+
+# produtos = 'Carbonato de sódio e óxido de zinco'
+# print('sódio' in produtos)
+# print('magnésio' not in produtos)
+
+# item = 'hipoclorito'
+# pos = item.find('clor')
+# print(pos)
+# pos = item.find('flu')
+# print(pos)
+
+# objeto_celeste = 'galaxia esPiral M31'
+# print(objeto_celeste.upper())           # Caixa alta
+# print(objeto_celeste.lower())           # Caixa baixa
+# print(objeto_celeste.capitalize())      # A primeira letra ficará maiúscula
+# print(objeto_celeste.title())          # A primeira letra de cada palavra ficará maiúscula
+
+# suplemento = 'cloreto de magnésio'
+# n_suplemento = suplemento.replace('magnésio', 'zinco')
+# print(suplemento)
+# print(n_suplemento)
+
+# frase = '       Ômega 3 é bom para a saúde!   '
+# print(frase)
+# print(frase.lstrip())
+# print(frase.rstrip())
+# print(frase.strip())
+
+# fruta = 'Abacate'
+# print(fruta)
+# print(fruta.rjust(20))
+# print(fruta.center(20))
+# print(fruta.ljust(20))
+# print(fruta.center(20, '-'))
+# print(fruta.ljust(20, '-'))
+
+# p = 'ARC Cloud Computing'
+# print(p.startswith('A'))
+# print(p.endswith('g'))
+
+# Docstrings
+'''
+Docstring é uma espécie de documentação que
+podemos inserir dentro de um módulo, função
+ou classe no Python, entre outros locais.
+    Respeita deslocamento do texto é multilinhas
+'''
+texto = '''
+Docstring é uma espécie de documentação que
+podemos inserir dentro de um módulo, função
+ou classe no Python, entre outros locais.
+    Respeita deslocamento do texto é multilinhas
+'''
+print(texto)
