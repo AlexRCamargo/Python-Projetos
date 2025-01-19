e00d7532e027b9b4c4fe6fd78cf959dd0584903b
AlexRCamargo <alexrcamargoti@gmail.com>
2025-01-19 11:27:27 -0300

    Aprendendo Python

--- arquivos.py
+++ arquivos.py
@@ -9,30 +9,49 @@ manipulador = open('arquivo.txt', 'r', encoding='utf-8') # Objeto
 # print(manipulador.readline())
 # print(manipulador.readline())
 
-# print(f'\nMétodo readlines():\n')
-# print(manipulador.readlines())
-# texto = input('Qual texto deseja procurar no arquivo? ')
-# try:
-#     manipulador = open('arquivo.txt', 'r', encoding='utf-8') # Objeto
-#     for linha in manipulador:
-#         linha = linha.rstrip()
-#         if texto in linha:
-#             print(f'A string foi encontrada!')
-#             print(linha)
-#         else:
-#             print(f'String não encontrada!')
-# except IOError:
-#     print(f'Não foi possivel abrir o arquivo') 
-# else:
-#     manipulador.close()
+print(f'\nMétodo readlines():\n')
+print(manipulador.readlines())
+texto = input('Qual texto deseja procurar no arquivo? ')
+try:
+    manipulador = open('arquivo.txt', 'r', encoding='utf-8') # Objeto
+    for linha in manipulador:
+        linha = linha.rstrip()
+        if texto in linha:
+            print(f'A string foi encontrada!')
+            print(linha)
+        else:
+            print(f'String não encontrada!')
+except IOError:
+    print(f'Não foi possivel abrir o arquivo') 
+else:
+    manipulador.close()
 
 # Escrever em arquivos de texto
 
+texto = '\nPython é usado em Ciência de Dados extensivamente.'
 try:
     manipulador = open('arquivo.txt', 'w', encoding='utf-8')
-    manipulador.write('ARC Cloud Computing\n')
-    manipulador.write('Como criar um arquivo de texto com Python.')
+    manipulador.write(texto)
+except IOError:
+    print(f'Não foi possivel abrir o arquivo') 
+else:
+    manipulador.close()
 
+frutas = ['Morango\n', 'Uva\n', 'Caju\n', 'Amora\n', 'Framboesa\n', 'Graviola']
+
+# Criar e Gravar o arquivo
+try:
+    manipulador = open('frutas.txt', 'w', encoding='utf-8')
+    manipulador.writelines(frutas)
+except IOError:
+    print(f'Não foi possivel abrir o arquivo') 
+else:
+    manipulador.close()
+
+#Ler o arquivo criado
+try:
+    manipulador = open('frutas.txt', 'r', encoding='utf-8')
+    print(manipulador.read())
 except IOError:
     print(f'Não foi possivel abrir o arquivo') 
 else:
