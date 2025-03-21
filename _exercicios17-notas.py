0136b41fbe24eea0283f5f5e4c963bfc572fda67
AlexRCamargo <alexrcamargoti@gmail.com>
2025-03-20 21:16:17 -0300

    Aprendendo Python

--- _exercicios17-notas.py
+++ _exercicios17-notas.py
@@ -9,19 +9,20 @@ aluno = input('Digite o nome do Aluno: ')
 disciplina = input('Digite o nome da matéria: ')
 
 while contador < 5:
-    notas.append(int(input(f'Informe a nota do {contador}º bimestre do aluno {aluno}: ')))
+    notas.append(float(input(f'Informe a nota do {contador}º bimestre do aluno {aluno}: ')))
     contador += 1
 
 media = sum(notas) / len(notas) # Função len indica quantos elementos tem dentro da lista
-print(f'A média final do {aluno} foi {media}')
+print(f'A média final de {aluno} foi {media}')
 if media >= 7:
     print(f'O aluno {aluno} foi APROVADO! Parabéns!!!')
 else:
-    notas.append(int(input('Informe a nota da prova final: ')))
+    print(f'O aluno {aluno} ficou de RECUPERAÇÃO! Estudo um pouco mais. Boa sorte!!!')
+    notas.append(float(input(f'Informe a nota da prova de Recuperação do aluno {aluno}: ')))
 
     media = sum(notas) / len(notas)
-    print(f'Sua média final foi {media}')
+    print(f'A média final de {aluno} foi {media}')
     if media >= 5:
-        print('APROVADO')
+        print(f'O aluno {aluno} foi APROVADO! Parabéns!!!')
     else:
-        print('REPROVADO')
+        print(f'O aluno {aluno} foi REPROVADO!')
